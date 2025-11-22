dff586f9c342c4104d57d60be533b7888a55a95b
hogyzen12 <billypapas12@gmail.com>
2025-11-22 18:12:39 +0100

    vercel a biatch

--- src/pages/MetaMePage.tsx
+++ src/pages/MetaMePage.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import HelioPayModal, { type HelioPayMethod } from "../components/HelioPayModal";
+import HelioPayModal from "../components/HelioPayModal";
 
 // Fixed product details
 const PLASTIC_MODEL = {
